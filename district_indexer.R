dba7bcb1983a09d6d4623720ccc4c449cf7d2282
monsieurcochon <jacobmulliken+github@gmail.com>
2018-11-24 18:25:52 -0500

    Changed paths for margin indexer

--- district_indexer.R
+++ district_indexer.R
@@ -1,4 +1,11 @@
 library(readr)
+
+house_results <- read_csv("Datasets/1976-2016-house.csv")
+
+houseResultsByYear <- function (x) {
+  result <- subset(house_results, year==x)
+  return(result)
+}
 compileMargins <- function (year) {
   yearlyResults <- houseResultsByYear(year)
   district_list <- unique(subset(yearlyResults,select=c(state,district)),incomparables = FALSE)
